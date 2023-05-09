4574759ffa43c583e2fb554e1ed04e2c5ffc2cc5
Dessie Kirrane <desmond.kirrane@gmail.com>
2023-05-09 12:36:03 +0100

    test

--- confluent/confluent-controlcenter.sh
+++ confluent/confluent-controlcenter.sh
@@ -5,5 +5,5 @@ set -e
 SCRIPT_PATH="$(dirname -- "${BASH_SOURCE[0]}")"
 
 # View Control Center
-kubectl -n confluent port-forward controlcenter-0 9021:9021
 sensible-browser http://localhost:9021
+kubectl -n confluent port-forward controlcenter-0 9021:9021
